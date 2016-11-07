99cbfd081d9693ef5dc8529664c3ab51530f1eb1
cesalberca <cesalberca@gmail.com>
2016-11-07 19:51:12 +0100

    Added final to the view and model. Also used equals to compare string.

--- src/es/cesalberca/calculator/controller/CalculatorController.java
+++ src/es/cesalberca/calculator/controller/CalculatorController.java
@@ -7,13 +7,14 @@ import es.cesalberca.calculator.view.CalculatorView;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Objects;
 
 /**
  * Created by Cesar
  */
 public class CalculatorController implements Observer {
-    private CalculatorView view;
-    private CalculatorModel model;
+    private final CalculatorView view;
+    private final CalculatorModel model;
 
     public CalculatorController (CalculatorView view, CalculatorModel model) {
         this.view = view;
@@ -36,7 +37,7 @@ public class CalculatorController implements Observer {
         public void actionPerformed(ActionEvent e) {
             String operationPressed = ((JButton)e.getSource()).getText();
 
-            if (operationPressed == "=") {
+            if (Objects.equals(operationPressed, "=")) {
                 model.setNumber(model.makeOperation());
             } else {
                 model.setCurrentTypeOfOperation(operationPressed);
