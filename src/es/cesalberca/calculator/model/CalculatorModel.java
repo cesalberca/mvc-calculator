02d7f13e75bce0e2cdae95e6ff4de08c28860f66
cesalberca <cesalberca@gmail.com>
2016-11-07 13:51:57 +0100

    Removed arraylist to implement adding digitis. Now it's just a number.

--- src/es/cesalberca/calculator/model/CalculatorModel.java
+++ src/es/cesalberca/calculator/model/CalculatorModel.java
@@ -4,7 +4,6 @@ import es.cesalberca.calculator.observer.Observer;
 import es.cesalberca.calculator.observer.Subject;
 
 import java.util.ArrayList;
-import java.util.Collections;
 
 /**
  * Created by Cesar
@@ -12,70 +11,47 @@ import java.util.Collections;
 public class CalculatorModel implements Subject {
     private ArrayList<Observer> observers;
 
-    private ArrayList<Integer> numbers;
     private double number;
     private double calculationValue;
 
     public CalculatorModel () {
         observers = new ArrayList<>();
-        numbers = new ArrayList<>();
+        number = 0;
     }
 
-    public void addTwoNumbers (int firstNumber, int secondNumber) {
-        this.setCalculationValue(firstNumber + secondNumber);
+    public void addDigit (float digit) {
+        setNumber((getNumber() * 10) + digit);
     }
 
-    public void addToNumbers (int number) {
-        this.numbers.add(number);
-        this.makeNumber();
-    }
-
-    public void makeNumber () {
-        double result = 0;
-        Double ten = 10.0;
-        Double dI;
-        ArrayList<Integer> reversedNumbers = new ArrayList<>(this.numbers);
-        Collections.reverse(reversedNumbers);
-
-        for (int i = 0; i < reversedNumbers.size(); i++) {
-            dI = i + 0.0;
-            result += reversedNumbers.get(i) * (Math.pow(ten, dI));
-        }
-
-        this.setNumber(result);
-    }
-
-    public double getCalculationValue() {
+    public double getCalculationValue () {
         return calculationValue;
     }
 
-    public void setCalculationValue(int calculationValue) {
+    public void setCalculationValue (int calculationValue) {
         this.calculationValue = calculationValue;
     }
 
-    public double getNumber() {
+    public double getNumber () {
         return number;
     }
 
-    public void setNumber(double number) {
+    public void setNumber (double number) {
         this.number = number;
         notifyObservers();
     }
 
     @Override
-    public void registerObserver(Observer o) {
+    public void registerObserver (Observer o) {
         observers.add(o);
     }
 
     @Override
-    public void unregisterObserver(Observer o) {
+    public void unregisterObserver (Observer o) {
         observers.remove(o);
     }
 
     @Override
     public void notifyObservers() {
-        for (Observer observer : this.observers) {
-            observer.update();
-        }
+        this.observers.forEach(Observer::update);
     }
 }
